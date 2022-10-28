22bd0c61f83eec821acc19a848e56984b2081814
Flávio Félix <fau_felix30@hotmail.com>
2022-10-28 12:48:47 -0300

    Configurando a aula 8.3

---
+++ aula_dia8.3/filter2.spec.js
@@ -0,0 +1,15 @@
+/* const { filterNoDriver, users } = require('./filter2');
+
+describe('The function filterNoDriver ', () => {
+  it('is a function', () => {
+    expect(typeof filterNoDriver).toBe('function');
+  });
+
+  it('returns people who are not drivers', () => {
+    expect(filterNoDriver(users)).toEqual([
+      { firstName: 'Bart', lastName: 'Simpson', isDriver: false },
+      { firstName: 'Lisa', lastName: 'Simpson', isDriver: false },
+      { firstName: 'Maggie', lastName: 'Simpson', isDriver: false }
+    ]);
+  });
+}); */
