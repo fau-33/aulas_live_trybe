acf90ef1bb9fce9a9ec00f7eb8ea8b9eb4559a77
Flávio Félix <fau_felix30@hotmail.com>
2022-10-28 04:28:23 -0300

    Configurando o setup para o dia 8.2

---
+++ aula_dia8.2/pizzas.js
@@ -0,0 +1,15 @@
+const pizzas = [
+  { flavor: 'Frango com Catupiry', price: 12.20 },
+  { flavor: 'Marguerita', price: 13.50},
+  { flavor: 'Gratinada', price: 15.32},
+  { flavor: 'Quatro Queijos', price: 13.50 },
+  { flavor: 'Xablau com um pouquinho assim de molho', price: 0 }
+];
+
+module.exports = {
+  infoPizzas,
+  priceLessThan,
+  everyPriceBiggerThan,
+  findPizza,
+  sortPizzas,
+};
