fbe36529b0a377840909f6662ebea5321d3a94ca
Flávio Félix <fau_felix30@hotmail.com>
2022-10-29 13:03:19 -0300

    Adiciona filterOdd

--- aula_dia8.3/filter1.js
+++ aula_dia8.3/filter1.js
@@ -1,6 +1,18 @@
+const filterOdd = (numbers) => {
+  return numbers.filter((number) => number % 2 !== 0);
+}
 
+//console.log(filterOdd([1, 2, 3, 4, 5]));
 
+// um modo de fazer
+/* const oddNumbers = [];
+  for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] % 2 !== 0){
+      oddNumbers.push(numbers[index]);
+    }
+  }
+  return oddNumbers; */
 
-/* module.exports = {
+module.exports = {
   filterOdd,
-} */
+}
